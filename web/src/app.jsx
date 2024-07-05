7ecc5fa6292b8e2ed4cc6c1125f471bf94f35a42
Polina Simonenko <rabarbrablad@gmail.com>
2024-07-05 18:46:32 +0200

    feat: added video

--- web/src/app.jsx
+++ web/src/app.jsx
@@ -2,14 +2,32 @@
 import "./public/globals.css"
 import ftReact from "@rabarbra/ft_react";
 
+const constraints = {
+    audio: false,
+    video: { width: 640, height: 360 },
+};
 
 const App = (props) => {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then((mediaStream) => {
+            const video = document.querySelector("video");
+            video.srcObject = mediaStream;
+            video.onloadedmetadata = () => {
+                video.play();
+            };
+        })
+        .catch((err) => {
+          // always check for errors at the end.
+            console.error(`${err.name}: ${err.message}`);
+        });
     return (
-        <div className="h-screen bg-slate-700 flex flex-row align-middle justify-center">
-            <h1 className="my-auto text-3xl">CAMAGURU</h1>
+        <div className="h-screen bg-slate-700 flex flex-col align-middle justify-center">
+            <h1 className="my-auto text-3xl text-center">CAMAGURU</h1>
+            <video></video>
         </div>
-    )
-}
+    );
+};
 
 const root = document.getElementById("app");
 ftReact.render(<App/>, root);
