5f096dfd2f65027642a7ab947fb0117b021f3600
Polina Simonenko <rabarbrablad@gmail.com>
2024-07-14 12:30:55 +0200

    feat: theme and font

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,4 +1,6 @@
 import daisyui from "daisyui"
+import typography from "@tailwindcss/typography"
+import defaultTheme from "tailwindcss/defaultTheme"
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -6,20 +8,60 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['"Kode Mono"', ...defaultTheme.fontFamily.sans],
+        // 'sans': ['"Syne Mono"', ...defaultTheme.fontFamily.sans],
+        // 'sans': ['"M PLUS 1 Code"', ...defaultTheme.fontFamily.sans],
+        // 'sans': ['"Cutive Mono"', ...defaultTheme.fontFamily.sans],
+        // 'sans': ['"Orbitron"', ...defaultTheme.fontFamily.sans],
+        // 'sans': ['"Gruppo"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [
+    typography,
     daisyui,
   ],
   daisyui: {
-    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "dark", // name of one of the included themes for dark mode
-    base: true, // applies background color and foreground color for root element by default
-    styled: true, // include daisyUI colors and design decisions for all components
-    utils: true, // adds responsive and modifier utility classes
-    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-    themeRoot: ":root", // The element that receives theme color CSS variables
+    themes: [
+        "nord",
+        {
+          nordDark: {
+            "color-scheme": "dark",
+            "primary": "#5E81AC",
+            "primary-content": "#03060b",
+            "secondary": "#8FBCBB",
+            "secondary-content": "#070d0d",
+            "accent": "#81A1C1",
+            "accent-content": "#06090e",
+            "neutral": "#3B4252",
+            "neutral-content": "#ECEFF4",
+            "base-100": "#2E3440",
+            "base-200": "#272c36",
+            "base-300": "#1f242d",
+            "base-content": "#d8dee9",
+            "info": "#88C0D0",
+            "info-content": "#070e10",
+            "success": "#A3BE8C",
+            "success-content": "#0a0d07",
+            "warning": "#EBCB8B",
+            "warning-content": "#130f07",
+            "error": "#BF616A",
+            "error-content": "#0d0304",
+            "--rounded-box": "0.4rem",
+            "--rounded-btn": "0.2rem",
+            "--rounded-badge": "0.4rem",
+            "--tab-radius": "0.2rem",
+          },
+      },
+    ],
+    darkTheme: "nordDark",
+    base: true,
+    styled: true,
+    utils: true,
+    prefix: "",
+    logs: true,
+    themeRoot: ":root",
   },
 }
-
